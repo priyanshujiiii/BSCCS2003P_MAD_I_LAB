76da20b2c861afed10eb86edcd9ec80ac67523a9
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-11-02 20:37:16 +0530

    Update 1) Intro to web app LAB.m

--- Introduction to the internet/1) Intro to web app LAB.m
+++ Introduction to the internet/1) Intro to web app LAB.m
@@ -3,3 +3,11 @@ What is the web app ?
 A web is a client server web application that runs in a web browser.
 
 wepapps = web application + websites + webpages
+
+2.1 Browser- web client
+A browser is a software which needs to be installed on a device to use webapps.
+
+Browser can make requests to a server using urls and render the response data
+
+moderns browser can interpet and runcode written by application author(javascript)
+Purpose: Make page interactive or dynamic.
