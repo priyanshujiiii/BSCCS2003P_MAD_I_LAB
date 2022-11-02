26429ea71ac10828a25ad5e984194ea26fb19bb9
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-11-02 21:09:33 +0530

    Create 2) Building a web-app.m

---
+++ Introduction to the internet/2) Building a web-app.m
@@ -0,0 +1,11 @@
+Building a web app that others can use?
+  
+  Building a webapp involves the following steps:
+
+
+1) Get a unique domain name
+a .  so that people remember your webapp so that can reach it.
+2) set up a computer with an IP to serve the webapp
+3) link the domain name with the server's ip address.
+4) write your application code(web server)
+5) Deploy your application code on the computer that is your serevr host
