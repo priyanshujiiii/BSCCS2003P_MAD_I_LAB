7db18ecc7052e06b70061067cc9045bda0d0a590
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-11-02 20:50:43 +0530

    Update 1) Intro to web app LAB.m

--- Introduction to the internet/1) Intro to web app LAB.m
+++ Introduction to the internet/1) Intro to web app LAB.m
@@ -11,3 +11,21 @@ Browser can make requests to a server using urls and render the response data
 
 moderns browser can interpet and runcode written by application author(javascript)
 Purpose: Make page interactive or dynamic.
+
+URL
+A url indentifies both the server and resources(webpage or content)
+url stands for uniform resources locator
+Also informally called "web addres"
+
+Protocol: It defines the modulw of communication between the client and the server for the request.
+Hostname: It identifies the server which will serve the request . It contains the domain name. In the case "flipkart".com
+path : it defines the resource which is bsing requested . it typically points to a webpage. (in this case list of books)
+Querystring: it typically contains any additional parameters that is typically used to add more context to
+the same path (in this case that are about"web app")
+
+https://www.flipkart.com/books/pr?sid=bks&web=app
+
+protocol: https://
+Hostname: www.flipkart.com
+path: books/pr
+Querystring: pr?sid=bks&web=app
