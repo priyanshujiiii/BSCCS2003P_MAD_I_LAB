0e0126e7c78d2849af54c1f8803e76a3d2c82068
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-11-02 21:11:40 +0530

    Update 2) Building a web-app.m

--- Introduction to the internet/2) Building a web-app.m
+++ Introduction to the internet/2) Building a web-app.m
@@ -9,3 +9,7 @@ a .  so that people remember your webapp so that can reach it.
 3) link the domain name with the server's ip address.
 4) write your application code(web server)
 5) Deploy your application code on the computer that is your serevr host
+
+3.1 Get a domain name
+
+3.2 Setup a serevr 
