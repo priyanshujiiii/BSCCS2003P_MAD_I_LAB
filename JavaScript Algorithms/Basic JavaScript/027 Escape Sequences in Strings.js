26f891b49b7d88439e5657fb37b7f3f2d6d342ab
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-11-27 19:42:22 +0530

    Create 027 Escape Sequences in Strings.js

---
+++ JavaScript Algorithms/Basic JavaScript/027 Escape Sequences in Strings.js
@@ -0,0 +1,12 @@
+Code	Output
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\t	tab
+\r	carriage return
+\b	word boundary
+\f	form feed
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; 
+// Change this line
