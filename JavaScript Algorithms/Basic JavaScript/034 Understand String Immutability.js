7d2734b44662a258ac84b15f8ab3701f1e9c6eb4
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-11-27 20:15:48 +0530

    Create 034 Understand String Immutability.js

---
+++ JavaScript Algorithms/Basic JavaScript/034 Understand String Immutability.js
@@ -0,0 +1,7 @@
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+var newstr = myStr.slice(1);
+myStr = "H" + newstr; // Change this line
+// Only change code above this line
