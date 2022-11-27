a17097981252a781f65ca25eaa2da28b2b1d2264
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-11-27 18:38:41 +0530

    Create 010 .js

---
+++ JavaScript Algorithms/Basic JavaScript/010 .js
@@ -0,0 +1,10 @@
+var fCC = "freeCodeCamp"; // Change this line
+var fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(fCC, fact); // Change this line
+
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
