4da824be18d76b10f579cacd13f532c2c8ea4aa9
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-11-28 00:09:26 +0530

    Create 077 Selecting from Many Options with Switch Statements.js

---
+++ JavaScript Algorithms/Basic JavaScript/077 Selecting from Many Options with Switch Statements.js
@@ -0,0 +1,18 @@
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val){
+    case 1 :
+    return "alpha";
+    break;
+    case 2 :
+    return "beta";
+    break;
+    case 3 :
+    return "gamma";
+    break;
+    case 4 :
+    return "delta";
+    break;
+  }
+    
