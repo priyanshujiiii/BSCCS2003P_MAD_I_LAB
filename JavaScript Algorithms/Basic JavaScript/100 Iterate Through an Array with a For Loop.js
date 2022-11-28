b9917917c0cd24010ff3e5f8facd10e041ca9262
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-11-28 13:24:32 +0530

    Create 100 Iterate Through an Array with a For Loop.js

---
+++ JavaScript Algorithms/Basic JavaScript/100 Iterate Through an Array with a For Loop.js
@@ -0,0 +1,8 @@
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+var total=0;
+// Only change code below this line
+for (let i = 0; i < myArr.length; i++) {
+  total += (myArr[i]);
+  console.log(myArr[i]);
+}
