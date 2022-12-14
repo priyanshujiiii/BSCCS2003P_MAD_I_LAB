3db39daa98e78679a5e3e8cd4f6e9f917c7c9a35
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-12-14 19:49:40 +0530

    Create 05 Serve JSON on a Specific Route.js

---
+++ Backend Development Apis/Basic Node and Express/05 Serve JSON on a Specific Route.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let app = express();
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"})
+})
