5e167f41d805e38d71ae2851b87a9725c663671a
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-12-14 19:43:28 +0530

    Create 04 Serve Static Assets.js

---
+++ Backend Development Apis/Basic Node and Express/04 Serve Static Assets.js
@@ -0,0 +1,7 @@
+let express = require('express');
+let app = express();
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
